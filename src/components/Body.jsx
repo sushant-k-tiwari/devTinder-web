afe87e3911161132f6c2b236b7a94ba7dad55c0a
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-07-24 22:31:32 +0530

    login with auth added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import { BASE_URL } from "../utils/contants";
+import axios from "axios";
+import { addUser } from "../utils/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.user);
+  
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <>
       <NavBar />
